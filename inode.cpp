6794de10ae5047b351f4f4c4bdd336c8b19eb7f9
Vatsal <sonivatsal111@gmail.com>
2018-11-11 19:14:36 +0530

    disk create and disk mount functions implemented

--- inode.cpp
+++ inode.cpp
@@ -8,79 +8,145 @@
 using namespace std;
 
 /******************************************************************************/
-static int active = 0;  /* is the virtual disk open (active) */
+static int active = 0; /* is the virtual disk open (active) */
 static int handle;
 
-char disk_name[50],filename[30];
+char disk_name[50], filename[30];
 struct super_block sb;
 struct file_to_inode_mapping file_inode_mapping_arr[NO_OF_INODES];
 struct inode inode_arr[NO_OF_INODES];
 
-map <string,int> dir_map; //file name as key maps to inode (value)
-vector<int> free_inode_vector; // denote free inodes
-vector<int> free_data_block_vector; // denote free data blocks
-vector<int> free_filedescriptor_vector; // denote free filedescriptor.
-int openfile_count=0 ; //keeps track of number of files opened.
-map <int,pair<int,int> > file_descriptor_map;	//Stores files Descriptor as key and corresponding Inode number(First) and file pointer.
+map<string, int> dir_map;                     //file name as key maps to inode (value)
+vector<int> free_inode_vector;                // denote free inodes
+vector<int> free_data_block_vector;           // denote free data blocks
+vector<int> free_filedescriptor_vector;       // denote free filedescriptor.
+int openfile_count = 0;                       //keeps track of number of files opened.
+map<int, pair<int, int>> file_descriptor_map; //Stores files Descriptor as key and corresponding Inode number(First) and file pointer.
 
-
+FILE *diskptr;
 /******************************************************************************/
-int create_disk(char *name)
-{ 
-  int f, cnt;
-  char buf[BLOCK_SIZE];
 
-  if (!name) {
-    fprintf(stderr, "make_disk: invalid file name\n");
-    return -1;
+int create_disk(char *disk_name)
+{
+  char buffer[BLOCK_SIZE];
+
+  diskptr = fopen(disk_name, "wb");
+  memset(buffer, 0, BLOCK_SIZE); // intialize null buffer
+
+  /* Using buffer to initialize whole disk as NULL  */
+  for (int i = 0; i < DISK_BLOCKS; ++i)
+    fwrite(buffer, 1, BLOCK_SIZE, diskptr);
+
+  struct super_block sb; //initializing sb
+
+  /* Marking all the blocks other than data blocks as used(super_block,mapping,inodes) */
+  for (int i = 0; i < sb.starting_index_of_data_blocks; ++i)
+    sb.datablock_freelist[i] = true; // true means reserved
+
+  /* Marking all data bloks as free */
+  for (int i = sb.starting_index_of_data_blocks; i < DISK_BLOCKS; ++i)
+    sb.datablock_freelist[i] = false; // false means free
+
+  /* marking all inodes as free */
+  for (int i = 0; i < NO_OF_INODES; ++i)
+    sb.inode_freelist[i] = false;
+
+  for (int i = 0; i < NO_OF_INODES; ++i)
+  {
+    /* setting all pointers of inode as free */
+    for (int j = 0; j < 13; j++)
+    {
+      inode_arr[i].pointer[j] = -1;
+    }
   }
 
-  if ((f = open(name, O_WRONLY | O_CREAT | O_TRUNC, 0644)) < 0) {
-    perror("make_disk: cannot open file");
-    return -1;
-  }
+  /* storing superblock into starting of the file */
+  fseek(diskptr, 0, SEEK_SET);
+  char sb_buff[sizeof(struct super_block)];
+  memset(sb_buff, 0, sizeof(struct super_block));
+  memcpy(sb_buff, &sb, sizeof(sb));
+  fwrite(sb_buff, sizeof(char), sizeof(sb), diskptr);
 
-  memset(buf, 0, BLOCK_SIZE);
-  for (cnt = 0; cnt < DISK_BLOCKS; ++cnt)
-    write(f, buf, BLOCK_SIZE);
+  /* storing file_inode_mapping after super block */
+  fseek(diskptr, (sb.no_of_blocks_used_by_superblock) * BLOCK_SIZE, SEEK_SET);
+  int len = sizeof(file_inode_mapping_arr);
+  char dir_buff[len];
+  memset(dir_buff, 0, len);
+  memcpy(dir_buff, file_inode_mapping_arr, len);
+  fwrite(dir_buff, sizeof(char), len, diskptr);
 
-  close(f);
+  /* storing inodes after file_inode mapping */
+  fseek(diskptr, (sb.starting_index_of_inodes) * BLOCK_SIZE, SEEK_SET);
+  int len = sizeof(inode_arr);
+  char inode_buff[len];
+  memset(inode_buff, 0, len);
+  memcpy(inode_buff, inode_arr, len);
+  fwrite(inode_buff, sizeof(char), len, diskptr);
 
-  return 0;
+  fclose(diskptr);
+  cout << "\nVirtual Disk Created!!!" << endl;
+
+  return 1;
 }
 
 int mount_disk(char *name)
 {
-  int f;
+  diskptr = fopen(disk_name,"rb+");
 
-  if (!name) {
-    fprintf(stderr, "open_disk: invalid file name\n");
-    return -1;
-  }  
-  
-  if (active) {
-    fprintf(stderr, "open_disk: disk is already open\n");
-    return -1;
-  }
-  
-  if ((f = open(name, O_RDWR, 0644)) < 0) {
-    perror("open_disk: cannot open file");
-    return -1;
-  }
+	/* retrieve super block from virtual disk and store into global struct super_block sb */
+	char sb_buff[sizeof(sb)];
+	memset(sb_buff,0,sizeof(sb));
+	fread(sb_buff,sizeof(char),sizeof(sb),diskptr);
+	memcpy(&sb,sb_buff,sizeof(sb));
 
-  handle = f;
-  active = 1;
+	/* retrieve file_inode mapping array from virtual disk and store into global
+  struct dir_entry file_inode_mapping_arr[NO_OF_INODES] */
+	fseek( diskptr, (sb.no_of_blocks_used_by_superblock)*BLOCK_SIZE, SEEK_SET );
+  int len = sizeof(file_inode_mapping_arr);
+	char dir_buff[len];
+	memset(dir_buff,0,len);
+	fread(dir_buff,sizeof(char),len,diskptr);
+	memcpy(file_inode_mapping_arr,dir_buff,len);
 
-  return 0;
+	/* retrieve Inode block from virtual disk and store into global struct inode 
+  inode_arr[NO_OF_INODES] */
+	fseek( diskptr, (sb.starting_index_of_inodes)*BLOCK_SIZE, SEEK_SET );
+  int len = sizeof(inode_arr);
+	char inode_buff[len];
+	memset(inode_buff,0,len);
+	fread(inode_buff,sizeof(char),len,diskptr);
+	memcpy(inode_arr,inode_buff,len);
+
+	/* storing all filenames into map */
+	for (int i = NO_OF_INODES - 1; i >= 0; --i)
+		if(sb.inode_freelist[i]==1)
+			dir_map[string(file_inode_mapping_arr[i].file_name)]=file_inode_mapping_arr[i].inode_num;
+		else
+			free_inode_vector.push_back(i);
+
+	/* maintain free data block vector */
+	for(int i = DISK_BLOCKS - 1; i>=sb.starting_index_of_data_blocks; --i)
+		if(sb.datablock_freelist[i] == 0)
+			free_data_block_vector.push_back(i);
+
+	/* maintain free filedescriptor vector */
+	for(int i=NO_OF_FILE_DESCRIPTORS-1;i>=0;i--){
+		free_filedescriptor_vector.push_back(i);
+	}
+
+  cout<<"Disk is mounted!!!"<<endl;
+
+	return 1;
 }
 
 int unmount_disk()
 {
-  if (!active) {
+  if (!active)
+  {
     fprintf(stderr, "close_disk: no open disk\n");
     return -1;
   }
-  
+
   close(handle);
 
   active = handle = 0;
@@ -90,22 +156,26 @@ int unmount_disk()
 
 int block_write(int block, char *buf)
 {
-  if (!active) {
+  if (!active)
+  {
     fprintf(stderr, "block_write: disk not active\n");
     return -1;
   }
 
-  if ((block < 0) || (block >= DISK_BLOCKS)) {
+  if ((block < 0) || (block >= DISK_BLOCKS))
+  {
     fprintf(stderr, "block_write: block index out of bounds\n");
     return -1;
   }
 
-  if (lseek(handle, block * BLOCK_SIZE, SEEK_SET) < 0) {
+  if (lseek(handle, block * BLOCK_SIZE, SEEK_SET) < 0)
+  {
     perror("block_write: failed to lseek");
     return -1;
   }
 
-  if (write(handle, buf, BLOCK_SIZE) < 0) {
+  if (write(handle, buf, BLOCK_SIZE) < 0)
+  {
     perror("block_write: failed to write");
     return -1;
   }
@@ -115,22 +185,26 @@ int block_write(int block, char *buf)
 
 int block_read(int block, char *buf)
 {
-  if (!active) {
+  if (!active)
+  {
     fprintf(stderr, "block_read: disk not active\n");
     return -1;
   }
 
-  if ((block < 0) || (block >= DISK_BLOCKS)) {
+  if ((block < 0) || (block >= DISK_BLOCKS))
+  {
     fprintf(stderr, "block_read: block index out of bounds\n");
     return -1;
   }
 
-  if (lseek(handle, block * BLOCK_SIZE, SEEK_SET) < 0) {
+  if (lseek(handle, block * BLOCK_SIZE, SEEK_SET) < 0)
+  {
     perror("block_read: failed to lseek");
     return -1;
   }
 
-  if (read(handle, buf, BLOCK_SIZE) < 0) {
+  if (read(handle, buf, BLOCK_SIZE) < 0)
+  {
     perror("block_read: failed to read");
     return -1;
   }
@@ -139,21 +213,21 @@ int block_read(int block, char *buf)
 }
 int main()
 {
-	string s;
-	cout<<"Enter disk name";
-	cin>>s;
-	char *ab = new char[s.length()+1];
-	strcpy(ab,s.c_str());
-	create_disk(ab);
-	mount_disk(ab);
-	int x;
-	while(1)
-	{
-		char* xx="aqw";
+  string s;
+  cout << "Enter disk name";
+  cin >> s;
+  char *ab = new char[s.length() + 1];
+  strcpy(ab, s.c_str());
+  create_disk(ab);
+  mount_disk(ab);
+  int x;
+  while (1)
+  {
+    char *xx = "aqw";
 
-		cin>>x;
-		if(x==1)
-			block_write(0,xx);
-	}
-	return 0;
+    cin >> x;
+    if (x == 1)
+      block_write(0, xx);
+  }
+  return 0;
 }
